7ecbe3a5adefcc20d2c6b09baa4e7f3f4a9b8df0
Pekarnik <pekgoglo@gmail.com>
2019-03-24 16:14:20 +0300

    remake

--- New Unity Project/Assets/Script/PanelController.cs
+++ New Unity Project/Assets/Script/PanelController.cs
@@ -4,69 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class PanelController : MonoBehaviour
+public class PanelController:MonoBehaviour
 {
-	[SerializeField]
-	private Transform _lowerPosition;
-	[SerializeField]
-	private Transform _middlePosition;
-	[SerializeField]
-	private Transform _upperPosition;
-	private bool _isTouched=false;
-	private float _speed=0.1f;
+	private GameObject _swapper;
+	private float _offset;
 	private void Start()
 	{
-		//transform.position = _lowerPosition.position;
-	}
-	private void OnMouseDrag()
-	{
-		_isTouched = true;
-		if (Input.touchCount==1)
-		{
-			Touch touch = Input.GetTouch(0);
-			Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-			touchPosition.z = 0f;
-			touchPosition.x = 0f;
-			if (transform.position.y >= _lowerPosition.position.y)
-			{
-				transform.position = touchPosition;
-			}
-			else
-			{
-				transform.position = new Vector3(0,_lowerPosition.position.y,0);
-			}
-		}
-	}
-
-	private void OnMouseUp()
-	{
-		_isTouched = false;
-		
+		_swapper = GameObject.FindGameObjectWithTag("Swapper");
+		_offset = transform.position.y - _swapper.transform.position.y;
 	}
 	private void Update()
 	{
-		if (!_isTouched)
-		{
-			
-			if (transform.position.y < _lowerPosition.position.y ||
-				Vector2.Distance(transform.position, _lowerPosition.position)
-				< Vector2.Distance(_middlePosition.position, transform.position))
-			{
-				transform.position = Vector2.Lerp(transform.position, _lowerPosition.position, _speed);
-			}
-			else if (Vector2.Distance(transform.position, _middlePosition.position)
-				< Vector2.Distance(_lowerPosition.position, transform.position) &&
-				Vector2.Distance(transform.position, _middlePosition.position)
-				< Vector2.Distance(_upperPosition.position, transform.position))
-			{
-				transform.position = Vector2.Lerp(transform.position, _middlePosition.position, _speed);
-			}
-			else if (transform.position.y > _upperPosition.position.y ||
-				Vector2.Distance(transform.position, _upperPosition.position)
-				< Vector2.Distance(_middlePosition.position, transform.position))
-			{
-				transform.position = Vector2.Lerp(transform.position, _upperPosition.position, _speed);
-			}
-		}
+		transform.position=Vector3.Lerp(transform.position,new Vector3(0, _swapper.transform.position.y + _offset),1);
 	}
 }
---
+++ New Unity Project/Assets/Script/SwapperController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class SwapperController : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _lowerPosition;
+	[SerializeField]
+	private Transform _middlePosition;
+	[SerializeField]
+	private Transform _upperPosition;
+	[HideInInspector]
+	public bool isTouched=false;
+	private float _speed=0.1f;
+	[HideInInspector]
+	public Position whereIsIt=Position.Lower;
+	private Vector3 _lower;
+	private Vector3 _middle;
+	private Vector3 _upper;
+	private void Start()
+	{
+		_lower = new Vector3(0, _lowerPosition.position.y, 0);
+		_middle = new Vector3(0, _middlePosition.position.y, 0);
+		_upper = new Vector3(0, _upperPosition.position.y, 0);
+	}
+	private void OnMouseDrag()
+	{
+		isTouched = true;
+		if (Input.touchCount==1)
+		{
+			Touch touch = Input.GetTouch(0);
+			Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+			touchPosition.z = 0f;
+			touchPosition.x = 0f;
+			if (transform.position.y >= _lower.y)
+			{
+				transform.position = touchPosition;
+			}
+			else
+			{
+				transform.position = _lower;
+			}
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		isTouched = false;
+		
+	}
+	private void Update()
+	{
+		if (!isTouched)
+		{
+			
+			if (transform.position.y < _lower.y ||
+				Vector2.Distance(transform.position, _lower)
+				< Vector2.Distance(_middle, transform.position))
+			{
+				transform.position = Vector2.Lerp(transform.position, _lower, _speed);
+				whereIsIt = Position.Lower;
+			}
+			else if (Vector2.Distance(transform.position, _middle)
+				< Vector2.Distance(_lowerPosition.position, transform.position) &&
+				Vector2.Distance(transform.position, _middle)
+				< Vector2.Distance(_upper, transform.position))
+			{
+				transform.position = Vector2.Lerp(transform.position, _middle, _speed);
+				whereIsIt = Position.Middle;
+			}
+			else if (transform.position.y > _upper.y ||
+				Vector2.Distance(transform.position, _upper)
+				< Vector2.Distance(_middle, transform.position))
+			{
+				transform.position = Vector2.Lerp(transform.position, _upper, _speed);
+				whereIsIt = Position.Upper;
+			}
+		}
+	}
+}
